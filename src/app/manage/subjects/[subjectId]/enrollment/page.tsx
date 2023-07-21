bf5dd5abb84cabd4578a85d29646e2a0ed5ef286
Geardevop <gearkiskeedev@gmail.com>
2023-07-21 10:50:22 +0700

    - setup branch

---
+++ src/app/manage/subjects/[subjectId]/enrollment/page.tsx
@@ -0,0 +1,11 @@
+import dynamic from 'next/dynamic'
+const page: React.FC<{ params: { subjectId: string, } }> = ({ params: { subjectId } }) => {
+  return (
+    <>
+        
+    </>
+  );
+  
+};
+
+export default page;
