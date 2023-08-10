31bbe031993f3c1ec63fe3efb078601e9cfcc54e
Geardevop <gearkiskeedev@gmail.com>
2023-08-10 21:05:37 +0700

    -add btn

--- src/app/manage/subjects/[subjectId]/approvalform/page.tsx
+++ src/app/manage/subjects/[subjectId]/approvalform/page.tsx
@@ -4,13 +4,13 @@ import React, { useMemo } from "react";
 import useGetEnroll from "@/core/hooks/enroll/useGetEnroll";
 import TableApprovalform from "./components/TableApprovalForm";
 import type { ResponseGetEnrollsType } from "@/app/api/enrolls/[subjectId]/EnrollType";
-
-
+import GoBackBtn from "@/core/components/GoBackBtn";
 const Page: React.FC<{ params: { subjectId: string } }> = ({ params: { subjectId } }) => {
   const getEnroll = useGetEnroll(subjectId);
   const enrolledStudents:ResponseGetEnrollsType = useMemo(() => getEnroll.data?.data.data || [], [getEnroll.data]);
   return (
     <>
+      <GoBackBtn/>
       <p className="text-sx m-4 font-bold text-center">สรุปข้อมูลขออนุมัตินักศึกษาช่วยงาน(รายวิชา)</p>
       <TableApprovalform  enrolledStudents={enrolledStudents}></TableApprovalform>
     </>
--- src/core/hooks/enroll/useGetEnroll.tsx
+++ src/core/hooks/enroll/useGetEnroll.tsx
@@ -3,11 +3,10 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { ResponseGetEnrollsType } from "@/app/api/enrolls/[subjectId]/EnrollType";
 
 const useGetEnroll = (subjectId: string) => {
-  const query =  useQuery<AxiosResponse<{ message: string; data: ResponseGetEnrollsType }>, AxiosError<{ message: string }>>({
+  return useQuery<AxiosResponse<{ message: string; data: ResponseGetEnrollsType }>, AxiosError<{ message: string }>>({
     queryKey: ["getEnrolls", subjectId],
     queryFn: () => axios.get(`/api/enrolls/${subjectId}`),
   });
-  return query
 };
 
 export default useGetEnroll;
