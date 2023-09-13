3c3a862a34f34d72122b3b5abffa8aceb05afbc8
Geardevop <gearkiskeedev@gmail.com>
2023-09-13 17:38:33 +0700

    -fix export mapper
    - create mapper Grade, degree, Majors to export

--- src/core/hooks/approvalForm/useCreateCsv.tsx
+++ src/core/hooks/approvalForm/useCreateCsv.tsx
@@ -1,6 +1,43 @@
+import { EnrollCourseFormType } from "@/app/api/subjects/studentView/[subjectId]/FullCourseWithEnrollStatusType";
 import type { FormatEnrolledData, StudentData, ApprovalFormFormattedType } from "./useFormatEnrolledData";
 
 const useCreateCsv= (enrolledStudentsFormatted: ApprovalFormFormattedType|undefined) => {
+  const degreeMapper: { label: string; value: EnrollCourseFormType["degree"] }[] = [
+    { label: "ปริญญาตรี", value: "BACHELOR_DEGREE" },
+    { label: "ปริญญาโท", value: "MASTER_DEGREE" },
+  ];
+  
+  const courseInMajorsMapper: { label: string; value: EnrollCourseFormType["courseInMajors"] }[] = [
+    { label: "สาขาวิชาเทคโนโลยีปัญญาประดิษฐ์", value: "ARTIFICIAL_INTELLIGENCE_TECHNOLOGY" },
+    {
+      label: "สาขาวิชาเทคโนโลยีสารสนเทศทางธุรกิจ (หลักสูตรนานาชาติ)",
+      value: "BUSINESS_INFORMATION_TECHNOLOGY_INTERNATIONAL_PROGRAM",
+    },
+    { label: "สาขาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ", value: "DATA_SCIENCE_AND_BUSINESS_ANALYTICS" },
+    { label: "สาขาวิชาเทคโนโลยีสารสนเทศ", value: "INFORMATION_TECHNOLOGY" },
+  ];
+  
+  const passedInMajorsMapper: { label: string; value: EnrollCourseFormType["passedInMajors"] }[] = [
+    { label: "สาขาวิชาเทคโนโลยีปัญญาประดิษฐ์", value: "ARTIFICIAL_INTELLIGENCE_TECHNOLOGY" },
+    {
+      label: "สาขาวิชาเทคโนโลยีสารสนเทศทางธุรกิจ (หลักสูตรนานาชาติ)",
+      value: "BUSINESS_INFORMATION_TECHNOLOGY_INTERNATIONAL_PROGRAM",
+    },
+    { label: "สาขาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ", value: "DATA_SCIENCE_AND_BUSINESS_ANALYTICS" },
+    { label: "สาขาวิชาเทคโนโลยีสารสนเทศ", value: "INFORMATION_TECHNOLOGY" },
+  ];
+  
+  const gradeMapper: { label: string; value: EnrollCourseFormType["grade"] }[] = [
+    { label: "A", value: "A" },
+    { label: "B+", value: "B_PLUS" },
+    { label: "B", value: "B" },
+    { label: "C+", value: "C_PLUS" },
+    { label: "C", value: "C" },
+    { label: "D+", value: "D_PLUS" },
+    { label: "D", value: "D" },
+    { label: "F", value: "F" },
+  ];
+  
   if(enrolledStudentsFormatted){
   let csvContent: string = "";
   // Header row
@@ -18,14 +55,14 @@ const useCreateCsv= (enrolledStudentsFormatted: ApprovalFormFormattedType|undefi
         isFirstRow = false;
         csvContent += `${index + 1},${approval.subjectId},${approval.courseNameEng},${approval.totalStudent},${
           approval.courseProfessor
-        },${student.id},${student.fullname},${student.degree},${student.courseInMajor},${student.passedInMajors},${
+        },${student.id},${student.fullname},${degreeMapper.find(item => item.value ===student.degree)?.label},${courseInMajorsMapper.find(item => item.value === student.courseInMajor)?.label},${passedInMajorsMapper.find(item => item.value === student.courseInMajor)?.label},${
           student.passedCourse
-        },${student.grade}\n`;
+        },${gradeMapper.find(item => item.value ===student.grade)?.label}\n`;
       } else {
         csvContent += `${index + 1},${approval.subjectId},${approval.courseNameEng},,,${student.id},${
           student.fullname
-        },${student.degree},${student.courseInMajor},${student.passedInMajors},${student.passedCourse},${
-          student.grade
+        },${degreeMapper.find(item => item.value ===student.degree)?.label},${courseInMajorsMapper.find(item => item.value === student.courseInMajor)?.label},${passedInMajorsMapper.find(item => item.value === student.courseInMajor)?.label},${student.passedCourse},${
+          gradeMapper.find(item => item.value ===student.grade)?.label
         }\n`;
       }
       if (index === rowspan - 1) {
